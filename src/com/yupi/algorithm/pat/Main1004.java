8eeb85190f8281948c8a9611b5437a663f5519d4
liyupi <592789970@qq.com>
2018-08-29 11:13:07 +0800

    +4

---
+++ src/com/yupi/algorithm/pat/Main1004.java
@@ -0,0 +1,104 @@
+package com.yupi.algorithm.pat;
+
+import java.util.Scanner;
+
+/**
+ * 功能描述：约会密码
+ *
+ * @author Yupi Li
+ * @date 2018/08/29 09:37
+ */
+public class Main1004
+{
+
+    static char[] getCommon(String a, String b)
+    {
+        char[] chars = new char[2];
+        int aLen = a.length();
+        int bLen = b.length();
+        int i = 0;
+        boolean first = true;
+        while (i < aLen && i < bLen)
+        {
+            char aa = a.charAt(i);
+            char bb = b.charAt(i);
+            if (first)
+            {
+                if (aa >= 'A' && aa <= 'Z' && bb >= 'A' && bb <= 'Z' && aa == bb)
+                {
+                    chars[0] = aa;
+                    first = false;
+                }
+            } else
+            {
+                if (aa == bb)
+                {
+                    chars[1] = aa;
+                    break;
+                }
+            }
+            i++;
+        }
+        return chars;
+    }
+
+    static int getSamePos(String c, String d)
+    {
+        int cLen = c.length();
+        int dLen = d.length();
+        int i = 0;
+        while (i < cLen && i < dLen)
+        {
+            char a = c.charAt(i);
+            char b = d.charAt(i);
+            if (a >= 'a' && a <= 'z' || a >= 'A' && a <= 'Z')
+            {
+            } else
+            {
+                i++;
+                continue;
+            }
+            if (a == b)
+            {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext())
+        {
+            String a = sc.nextLine();
+            String b = sc.nextLine();
+            String c = sc.nextLine();
+            String d = sc.nextLine();
+            char[] chars = getCommon(a, b);
+            int pos = getSamePos(c, d);
+            String posStr;
+            if (pos < 10)
+            {
+                posStr = "0" + pos;
+            } else
+            {
+                posStr = String.valueOf(pos);
+            }
+            int hour;
+            String hourStr;
+            if (chars[1] >= '0' && chars[1] <= '9')
+            {
+                hour = chars[1] - 48;
+                hourStr = "0" + hour;
+            } else
+            {
+                hour = chars[1] - 64 + 9;
+                hourStr = String.valueOf(hour);
+            }
+            String[] dates = new String[]{"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+            System.out.println(dates[chars[0] - 65] + " " + hourStr + ":" + posStr);
+        }
+    }
+}
---
+++ src/com/yupi/algorithm/pat/Main1005.java
@@ -0,0 +1,119 @@
+package com.yupi.algorithm.pat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * 功能描述：德才论（狗题，浪费时间）
+ *
+ * @author Yupi Li
+ * @date 2018/08/29 10:59
+ */
+public class Main1005
+{
+    public static void main(String[] args)
+    {
+        class Student
+        {
+            private String ID;
+            private int morals;
+            private int talents;
+
+            Student(String ID, int morals, int talents)
+            {
+                this.ID = ID;
+                this.morals = morals;
+                this.talents = talents;
+            }
+        }
+
+        ArrayList<Student> firstClass = new ArrayList<Student>();
+        ArrayList<Student> secondClass = new ArrayList<Student>();
+        ArrayList<Student> thirdClass = new ArrayList<Student>();
+        ArrayList<Student> fourthClass = new ArrayList<Student>();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int L = sc.nextInt();
+        int H = sc.nextInt();
+        Student[] student = new Student[num];
+        for (int i = 0; i < num; i++)
+        {
+            student[i] = new Student(sc.next(), sc.nextInt(), sc.nextInt());
+            if (student[i].morals >= H && student[i].talents >= H)
+                firstClass.add(student[i]);
+            else if (student[i].morals >= H && student[i].talents >= L)
+                secondClass.add(student[i]);
+            else if (student[i].morals >= L && student[i].talents >= L && student[i].morals >= student[i].talents)
+                thirdClass.add(student[i]);
+            else if (student[i].morals >= L && student[i].talents >= L)
+                fourthClass.add(student[i]);
+        }
+        System.out.println(firstClass.size() + secondClass.size() + thirdClass.size() + fourthClass.size());
+        Comparator<Student> com = (o1, o2) -> {
+            if (o1.morals + o1.talents > o2.morals + o2.talents)
+            {
+                return -1;
+            } else if (o1.morals + o1.talents < o2.morals + o2.talents)
+            {
+                return 1;
+            } else
+            {
+                if (o1.morals > o2.morals)
+                {
+                    return -1;
+                } else if (o1.morals < o2.morals)
+                {
+                    return 1;
+                } else
+                {
+                    if (Integer.valueOf(o1.ID) < Integer.valueOf(o2.ID))
+                    {
+                        return -1;
+                    } else
+                    {
+                        return 1;
+                    }
+                }
+            }
+        };
+        Collections.sort(firstClass, com);
+        Collections.sort(secondClass, com);
+        Collections.sort(thirdClass, com);
+        Collections.sort(fourthClass, com);
+        for (int i = 0; i < firstClass.size(); i++)
+        {
+            String id = firstClass.get(i).ID;
+            int m = firstClass.get(i).morals;
+            int t = firstClass.get(i).talents;
+            System.out.println(id + " " + m + " " + t);
+        }
+        for (int i = 0; i < secondClass.size(); i++)
+        {
+            String id = secondClass.get(i).ID;
+            int m = secondClass.get(i).morals;
+            int t = secondClass.get(i).talents;
+            System.out.println(id + " " + m + " " + t);
+        }
+
+        for (int i = 0; i < thirdClass.size(); i++)
+        {
+            String id = thirdClass.get(i).ID;
+            int m = thirdClass.get(i).morals;
+            int t = thirdClass.get(i).talents;
+            System.out.println(id + " " + m + " " + t);
+        }
+
+        for (int i = 0; i < fourthClass.size(); i++)
+        {
+            String id = fourthClass.get(i).ID;
+            int m = fourthClass.get(i).morals;
+            int t = fourthClass.get(i).talents;
+            System.out.println(id + " " + m + " " + t);
+        }
+
+
+    }
+
+}
