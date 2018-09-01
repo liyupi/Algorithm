0b9967a60099db78b9a2697f811c39723cc3fb20
liyupi <592789970@qq.com>
2018-09-01 09:17:09 +0800

    +1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 /**
- * 功能描述：二叉树的深度（自顶向下和自底向上）
+ * 功能描述：二叉树是否对称（递归）
  *
  * @author Yupi Li
  * @date 2018/08/29 09:37
  */
 public class Main
 {
+    public static void main(String args[])
+    {
+        StringBuilder stringBuilder = new StringBuilder("AAB");
+        System.out.println(stringBuilder.reverse());
+        System.out.println(stringBuilder);
+    }
+
+    public boolean isSymmetric(TreeNode root)
+    {
+        if (root == null){
+            return true;
+        }
+        return isSymmetric(root.left,root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right)
+    {
+        if (left == null && right == null)
+        {
+            return true;
+        }
+        if (left == null || right == null)
+        {
+            return false;
+        }
+        return left.val==right.val&&isSymmetric(left.left,right.right)&&isSymmetric(left.right,right.left);
+    }
+
     public class TreeNode
     {
         int val;
@@ -20,39 +44,5 @@ public class Main
         {
             val = x;
         }
-    }
-
-
-    /**
-     * 自底向上
-     **/
-    public int maxDepth(TreeNode root) {
-        if (root == null){
-            return 0;
-        }
-        int x = maxDepth(root.left);
-        int y = maxDepth(root.right);
-        return Math.max(x,y)+1;
-    }
-
-    /**
-     * 自顶向下
-     **/
-    int answer = 0;
-    public int getMaxDepth(TreeNode root,int depth){
-        if (root == null){
-            if (depth>answer){
-                answer = depth;
-            }
-            return depth;
-        }
-        getMaxDepth(root.left,depth+1);
-        getMaxDepth(root.right,depth+1);
-        return answer;
-    }
-
-    public static void main(String args[])
-    {
-
     }
 }
