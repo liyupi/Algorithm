c635259d58c7196f100918f5b2a53b4fbcba97f0
liyupi <592789970@qq.com>
2018-09-08 09:40:44 +0800

    +1

--- src/com/yupi/algorithm/pat/simple_practice/Main1027.java
+++ src/com/yupi/algorithm/pat/simple_practice/Main1027.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.pat.simple_practice;
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -7,7 +9,7 @@ import java.util.Scanner;
  * @author Yupi Li
  * @date 2018/08/29 09:37
  */
-public class Main {
+public class Main1027 {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
