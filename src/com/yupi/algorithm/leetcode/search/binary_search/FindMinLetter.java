58240f7b793dfe5a285204fff2cec0048e3a81ba
liyupi <592789970@qq.com>
2018-09-11 08:44:53 +0800

    +1

--- src/com/yupi/algorithm/leetcode/search/binary_search/FindMinLetter.java
+++ src/com/yupi/algorithm/leetcode/search/binary_search/FindMinLetter.java
@@ -1,13 +1,15 @@
+package com.yupi.algorithm.leetcode.search.binary_search;
+
 /**
  * 功能描述：寻找比目标字母大的最小字母
  *
  * @author Yupi Li
  * @date 2018/08/29 09:37
  */
-public class Main {
+public class FindMinLetter {
 
     public static void main(String[] args) {
-        System.out.println(new Main().nextGreatestLetter(new char[]{'c', 'f', 'g'}, 'k'));
+        System.out.println(new FindMinLetter().nextGreatestLetter(new char[]{'c', 'f', 'g'}, 'k'));
     }
 
     public char nextGreatestLetter(char[] letters, char target) {
