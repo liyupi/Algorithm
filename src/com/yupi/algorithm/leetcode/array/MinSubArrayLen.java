3c1df432d1070191e94581e043965e078f9cb402
liyupi <592789970@qq.com>
2018-09-17 22:41:50 +0800

    +1

--- src/com/yupi/algorithm/leetcode/array/MinSubArrayLen.java
+++ src/com/yupi/algorithm/leetcode/array/MinSubArrayLen.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+package com.yupi.algorithm.leetcode.array;
 
 /**
  * 功能描述：寻找其和大于等于s的长度最小连续子数组
@@ -9,10 +7,10 @@ import java.util.Scanner;
  * @date 2018/08/29 09:37
  */
 
-public class Main {
+public class MinSubArrayLen {
 
     public static void main(String[] args) {
-        System.out.println(new Main().minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
+        System.out.println(new MinSubArrayLen().minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
     }
 
     public int minSubArrayLen(int s, int[] nums) {
