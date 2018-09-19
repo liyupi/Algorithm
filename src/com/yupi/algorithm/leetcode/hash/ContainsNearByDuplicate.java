06aefb5496df8d60772c96520a7ea12d7bfc3bea
liyupi <592789970@qq.com>
2018-09-19 09:56:52 +0800

    +1

--- src/com/yupi/algorithm/leetcode/hash/ContainsNearByDuplicate.java
+++ src/com/yupi/algorithm/leetcode/hash/ContainsNearByDuplicate.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.hash;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +10,7 @@ import java.util.Map;
  * @date 2018/08/29 09:37
  */
 
-public class Main {
+public class ContainsNearByDuplicate {
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
