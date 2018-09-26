108b666ebdf9e2b4c31a01b7627fb2496c6cb53a
liyupi <592789970@qq.com>
2018-09-26 23:49:27 +0800

    +1

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,40 @@
-import java.util.*;
-
 /**
- * 功能描述：单词搜索II
+ * 功能描述：最长回文子串（动态规划）
  *
  * @author Yupi Li
  * @date 2018/08/29 09:37
  */
 
 public class Main {
-
-    private Set<String> res = new HashSet<>();
-    private Node root = new Node();
-    private int[][] dir = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    void insert(String str) {
-        int len = str.length();
-        Node node = root;
-        for (int i = 0; i < len; i++) {
-            char c = str.charAt(i);
-            if (!node.children.containsKey(c)) {
-                node.children.put(c, new Node());
-            }
-            node = node.children.get(c);
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len == 0){
+            return "";
         }
-        node.word = str;
-    }
-
-    void dfs(char[][] board, int x, int y, Node node, boolean[][] visited) {
-        visited[x][y] = true;
-        if (node.children.containsKey(board[x][y])) {
-            String word = node.children.get(board[x][y]).word;
-            if (word != null) {
-                res.add(word);
+        int left = 0;
+        int right = 0;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len - 1; i++) {
+            dp[i][i] = 1;
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = 1;
+                left = i;
+                right = i + 1;
             }
-            for (int i = 0; i < dir.length; i++) {
-                int tempX = x + dir[i][0];
-                int tempY = y + dir[i][1];
-                if (tempX < board.length && tempX >= 0 && tempY < board[tempX].length && tempY >= 0 && !visited[tempX][tempY]) {
-                    dfs(board, tempX, tempY, node.children.get(board[x][y]), visited);
+        }
+        dp[len - 1][len - 1] = 1;
+        for (int l = 3; l <= len; l++) {
+            for (int i = 0; i + l <= len; i++) {
+                int j = i + l - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                    if (dp[i][j] == 1) {
+                        left = i;
+                        right = j;
+                    }
                 }
             }
         }
-        visited[x][y] = false;
-    }
-
-    public List<String> findWords(char[][] board, String[] words) {
-        int len = words.length;
-        for (int i = 0; i < len; i++) {
-            insert(words[i]);
-        }
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                dfs(board, i, j, root, visited);
-            }
-        }
-        return new ArrayList<>(res);
-    }
-
-    class Node {
-        Map<Character, Node> children = new HashMap<>();
-        String word;
-    }
-
-    public static void main(String[] args) {
-        new Main().findWords(new char[][]{{'a', 'b'}, {'a', 'a'}}, new String[]{"aba", "baa", "bab", "aaab", "aaa", "aaaa", "aaba"});
+        return s.substring(left, right + 1);
     }
 }
