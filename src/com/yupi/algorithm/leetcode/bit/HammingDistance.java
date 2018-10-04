fabd8651fef187833031dad59237b665ac513dba
liyupi <592789970@qq.com>
2018-10-04 15:56:22 +0800

    +1

--- src/com/yupi/algorithm/leetcode/bit/HammingDistance.java
+++ src/com/yupi/algorithm/leetcode/bit/HammingDistance.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.bit;
+
 /**
  * 功能描述：两个二进制数的不同位数
  *
@@ -5,7 +7,7 @@
  * @date 2018/10/04 09:37
  */
 
-public class Main {
+public class HammingDistance {
 
     public int hammingDistance(int x, int y) {
         int num = x ^ y;
