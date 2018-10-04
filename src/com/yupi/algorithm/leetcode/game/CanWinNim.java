8d0320abf1de74ba727fcfd3b212c002a1b1aa3c
liyupi <592789970@qq.com>
2018-10-04 10:19:20 +0800

    +1

--- src/com/yupi/algorithm/leetcode/game/CanWinNim.java
+++ src/com/yupi/algorithm/leetcode/game/CanWinNim.java
@@ -1,9 +1,11 @@
+package com.yupi.algorithm.leetcode.game;
+
 /**
  * 功能描述：Nim 游戏
  *
  */
 
-public class Main {
+public class CanWinNim {
 
     public boolean canWinNim(int n) {
         return n % 4 != 0;
