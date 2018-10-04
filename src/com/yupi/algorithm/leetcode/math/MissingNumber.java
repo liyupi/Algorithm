13ad9b3e46dff83ae489d150c7c57a4833ff3b92
liyupi <592789970@qq.com>
2018-10-04 15:48:16 +0800

    +1

--- src/com/yupi/algorithm/leetcode/math/MissingNumber.java
+++ src/com/yupi/algorithm/leetcode/math/MissingNumber.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.math;
+
 /**
  * 功能描述：寻找缺失的数
  *
@@ -5,7 +7,7 @@
  * @date 2018/10/04 09:37
  */
 
-public class Main {
+public class MissingNumber {
 
     public int missingNumber(int[] nums) {
         long sum = nums.length * (nums.length + 1) / 2;
