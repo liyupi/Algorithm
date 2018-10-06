bc0e6cf3c21ece653828f30cddbd84c4940dc689
liyupi <592789970@qq.com>
2018-10-06 09:26:36 +0800

    +1

--- src/com/yupi/algorithm/leetcode/linkedlist/DeleteDuplicate.java
+++ src/com/yupi/algorithm/leetcode/linkedlist/DeleteDuplicate.java
@@ -1,15 +1,15 @@
-import com.yupi.algorithm.leetcode.linkedlist.ListNode;
+package com.yupi.algorithm.leetcode.linkedlist;
 
 /**
  * 功能描述：删除链表中重复节点
- *
+ * 
  * 注意陷阱：如"aaa   "
  *
  * @author Yupi Li
  * @date 2018/10/04 09:37
  */
 
-public class Main {
+public class DeleteDuplicate {
 
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) {
