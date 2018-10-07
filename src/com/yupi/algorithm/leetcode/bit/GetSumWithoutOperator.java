3acf6e604a74c0dc2dbbf9c0a39c94bbd35751a5
liyupi <592789970@qq.com>
2018-10-07 08:54:15 +0800

    +1

--- src/com/yupi/algorithm/leetcode/bit/GetSumWithoutOperator.java
+++ src/com/yupi/algorithm/leetcode/bit/GetSumWithoutOperator.java
@@ -1,11 +1,15 @@
+package com.yupi.algorithm.leetcode.bit;
+
 /**
  * 功能描述：模拟求和
+ *
+ * 思路：用^来模拟加法，&来模拟进位
  *
  * @author Yupi Li
  * @date 2018/10/04 09:37
  */
 
-public class Main {
+public class GetSumWithoutOperator {
 
     public int getSum(int a, int b) {
         if (b == 0) {
