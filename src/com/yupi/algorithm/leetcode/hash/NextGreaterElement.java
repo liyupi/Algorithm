40174f4d9dd16664accdd5efd0079008f2ec48cc
liyupi <592789970@qq.com>
2018-10-14 10:47:01 +0800

    +1

--- src/com/yupi/algorithm/leetcode/hash/NextGreaterElement.java
+++ src/com/yupi/algorithm/leetcode/hash/NextGreaterElement.java
@@ -1,8 +1,11 @@
 package com.yupi.algorithm.leetcode.hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 功能描述：下一个最大元素
- *
+ * <p>
  * 思路：用map来记录每个元素出现的位置，加速初始定位
  *
  * @author Yupi Li
@@ -12,20 +15,21 @@ package com.yupi.algorithm.leetcode.hash;
 public class NextGreaterElement {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
         int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            boolean start = false;
-            for (int j = 0; j < nums2.length; j++) {
-                if (start && nums1[i] < nums2[j]) {
-                    res[i] = nums2[j];
-                    start = false;
+            int start = map.get(nums1[i]) + 1;
+            while (start < nums2.length) {
+                if (nums2[start] > nums1[i]) {
+                    res[i] = nums2[start];
                     break;
                 }
-                if (nums1[i] == nums2[j]) {
-                    start = true;
-                }
+                start++;
             }
-            if (start) {
+            if (start == nums2.length) {
                 res[i] = -1;
             }
         }
