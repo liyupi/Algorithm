dcaee5b736ef790b68ffb5d93b10f0573dc6a601
liyupi <592789970@qq.com>
2018-10-16 09:33:57 +0800

    +1

--- src/com/yupi/algorithm/leetcode/array/FindRelativeRanks.java
+++ src/com/yupi/algorithm/leetcode/array/FindRelativeRanks.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.array;
+
 /**
  * 功能描述：相对排名
  *
@@ -11,7 +13,7 @@
  * @date 2018/10/11 09:37
  */
 
-public class Main {
+public class FindRelativeRanks {
 
     public String[] findRelativeRanks(int[] nums) {
         int max = 0;
