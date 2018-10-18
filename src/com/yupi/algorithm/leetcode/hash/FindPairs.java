2c0d39e7f8d91d1f02bc8253aca045a311fa2083
liyupi <592789970@qq.com>
2018-10-18 09:09:26 +0800

    +1

--- src/com/yupi/algorithm/leetcode/hash/FindPairs.java
+++ src/com/yupi/algorithm/leetcode/hash/FindPairs.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.hash;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +15,7 @@ import java.util.Map;
  * @date 2018/10/11 09:37
  */
 
-public class Main {
+public class FindPairs {
 
     public int findPairs(int[] nums, int k) {
         if (k < 0) {
