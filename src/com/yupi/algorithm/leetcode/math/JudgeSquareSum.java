bf0fc9e121557a1cc5d4640081a4d00ec5408166
liyupi <592789970@qq.com>
2018-10-23 09:40:48 +0800

    +1

--- src/com/yupi/algorithm/leetcode/math/JudgeSquareSum.java
+++ src/com/yupi/algorithm/leetcode/math/JudgeSquareSum.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.math;
+
 /**
  * 功能描述：平方数之和
  *
@@ -7,7 +9,7 @@
  * @date 2018/10/11 09:37
  */
 
-public class Main {
+public class JudgeSquareSum {
 
     public boolean judgeSquareSum(int c) {
         int max = (int) Math.sqrt(c / 2);
