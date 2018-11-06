974a4d34a32b9a15761beb8c1e2fb3bf43b7872b
liyupi <592789970@qq.com>
2018-11-06 11:09:46 +0800

    +1

--- src/com/yupi/algorithm/leetcode/bit/BinaryGap.java
+++ src/com/yupi/algorithm/leetcode/bit/BinaryGap.java
@@ -1,10 +1,12 @@
+package com.yupi.algorithm.leetcode.bit;
+
 /**
  * 功能描述：二进制最大间距
  * <p>
  * 思路：算术右移，依次判断每一位是否为1，双指针记录间距
  */
 
-public class Main {
+public class BinaryGap {
 
     public int binaryGap(int N) {
         int left = -1;
