1f4533c14a8e688c1b0f45957dcbdb9fac18cf16
liyupi <592789970@qq.com>
2018-11-06 10:07:58 +0800

    +1

--- src/com/yupi/algorithm/leetcode/matrix/NumMagicSquaresInside.java
+++ src/com/yupi/algorithm/leetcode/matrix/NumMagicSquaresInside.java
@@ -1,32 +1,69 @@
 package com.yupi.algorithm.leetcode.matrix;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * 功能描述：比较含退格的字符串
+ * 功能描述：矩阵中的幻方
  *
- * 思路：比较压缩后的字符串是否相等即可
+ * 思路：如果中间的数不为5，则必定不是幻方
  */
 
 public class NumMagicSquaresInside {
 
-    public boolean backspaceCompare(String S, String T) {
-        return compress(S).equals(compress(T));
+    public int numMagicSquaresInside(int[][] grid) {
+        int sum = 0;
+        for (int i = 2; i < grid.length; i++) {
+            for (int j = 2; j < grid[0].length; j++) {
+                if (isMagicSquare(grid,i,j)) {
+                    sum++;
+                }
+            }
+        }
+        return sum;
     }
 
-    String compress(String s) {
-        int delete = 0;
-        int pos = s.length() - 1;
-        String res = "";
-        while (pos >= 0) {
-            if (s.charAt(pos) != '#' && delete == 0) {
-                res = s.charAt(pos) + res;
-            } else if (s.charAt(pos) == '#') {
-                delete++;
-            } else {
-                delete--;
+    private boolean isMagicSquare(int[][] grid, int i, int j) {
+        Set<Integer> set = new HashSet<>();
+        for (int k = i - 2; k <= i; k++) {
+            for (int l = j - 2; l <= j; l++) {
+                if (grid[k][l] > 9 || grid[k][l] < 1) {
+                    return false;
+                }
+                set.add(grid[k][l]);
             }
-            pos--;
         }
-        return res;
+        if (set.size() != 9) {
+            return false;
+        }
+        if (grid[i - 1][j - 1] != 5) {
+            return false;
+        }
+        if (grid[i - 2][j - 2] + grid[i - 2][j - 1] + grid[i - 2][j] != 15) {
+            return false;
+        }
+        if (grid[i - 1][j - 2] + grid[i - 1][j] != 10) {
+            return false;
+        }
+        if (grid[i][j - 2] + grid[i][j - 1] + grid[i][j] != 15) {
+            return false;
+        }
+        if (grid[i][j] + grid[i - 1][j] + grid[i - 2][j] != 15) {
+            return false;
+        }
+        if (grid[i - 2][j - 1] + grid[i][j - 1] != 10) {
+            return false;
+        }
+        if (grid[i][j - 2] + grid[i - 1][j - 2] + grid[i - 2][j - 2] != 15) {
+            return false;
+        }
+        if (grid[i][j] + grid[i - 2][j - 2] != 10) {
+            return false;
+        }
+        if (grid[i][j - 2] + grid[i - 2][j] != 10) {
+            return false;
+        }
+        return true;
     }
 
 }
