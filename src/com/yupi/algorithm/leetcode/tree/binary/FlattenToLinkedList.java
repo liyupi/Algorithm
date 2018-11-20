2f811e6929c6d8c9d7ecaf9d4b71a0c8fb1472fb
liyupi <592789970@qq.com>
2018-11-20 09:25:43 +0800

    +1

--- src/com/yupi/algorithm/leetcode/tree/binary/FlattenToLinkedList.java
+++ src/com/yupi/algorithm/leetcode/tree/binary/FlattenToLinkedList.java
@@ -1,3 +1,5 @@
+package com.yupi.algorithm.leetcode.tree.binary;
+
 import com.yupi.algorithm.leetcode.tree.bst.TreeNode;
 
 /**
@@ -6,7 +8,7 @@ import com.yupi.algorithm.leetcode.tree.bst.TreeNode;
  * 思路：递归，需要记录左子树最右的值，注意左右子树都要递归
  */
 
-public class Main {
+public class FlattenToLinkedList {
 
     TreeNode last;
 
