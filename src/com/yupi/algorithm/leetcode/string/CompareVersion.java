b00b7cab72497d122d0f51cc354844d8b6fac2af
liyupi <592789970@qq.com>
2018-11-26 10:32:36 +0800

    +1

--- src/com/yupi/algorithm/leetcode/string/CompareVersion.java
+++ src/com/yupi/algorithm/leetcode/string/CompareVersion.java
@@ -1,10 +1,12 @@
+package com.yupi.algorithm.leetcode.string;
+
 /**
  * 功能描述：比较版本号
  *
  * 思路：特殊情况较多，数范围小时可直接将整个串表示成数，否则拆分成字符串数组依次比较
  */
 
-public class Main {
+public class CompareVersion {
 
     public int compareVersion(String version1, String version2) {
         String[] words1 = version1.split("\\.");
