4fb8ebba294aefb888cd9f5b89ec64e2fd10f7d2
liyupi <592789970@qq.com>
2018-11-29 20:36:33 +0800

    +1

--- src/com/yupi/algorithm/leetcode/math/trap.java
+++ src/com/yupi/algorithm/leetcode/math/trap.java
@@ -1,10 +1,12 @@
+package com.yupi.algorithm.leetcode.math;
+
 /**
  * 功能描述：接雨水
  * 思路：如果一味从左到右或从右到左记录最大值后累加求和，会被最高的位置干扰导致少算一边
  * 因此需要先找到最高的位置，再分别对从左到右和从右到左累加求和
  */
 
-public class Main {
+public class trap {
 
     public int trap(int[] height) {
         if (height.length == 0) {
