1f5cb3873f91bf3f2d5ec1e39b54e96e4354ea29
liyupi <592789970@qq.com>
2018-12-02 09:29:04 +0800

    +1

--- src/com/yupi/algorithm/leetcode/math/ComputeArea.java
+++ src/com/yupi/algorithm/leetcode/math/ComputeArea.java
@@ -1,10 +1,12 @@
+package com.yupi.algorithm.leetcode.math;
+
 /**
  * 功能描述：矩形面积
  * 思路：先找到不重叠的情况，4种
  * 再找到重叠矩形的左下角坐标和右上角坐标，总面积=S1+S2-重叠
  */
 
-public class Main {
+public class ComputeArea {
 
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         int sum = (D - B) * (C - A) + (G - E) * (H - F);
